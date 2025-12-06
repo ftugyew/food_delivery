89d6a58e82b8034444f67e2e9e1a8bccb69ea88a
Sravan Kumar <sravankumar64099@gmail.com>
2025-12-06 12:04:03 +0530

    Fix script type and remove /ping test

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -5,18 +5,3 @@ if (typeof window !== "undefined") {
   window.API_BASE_URL = API_BASE_URL;
 }
 
-// Simple /ping test for debugging
-try {
-  fetch(`${API_BASE_URL}/ping`)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log("[Tindo API /ping]", data);
-    })
-    .catch((err) => {
-      console.warn("[Tindo API /ping] failed", err);
-    });
-} catch (err) {
-  console.warn("[Tindo API /ping] fetch not available", err);
-}
-
-
